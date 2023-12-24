610eff6410b337e1db486dd5ce9a7c7edee8e84d
vgvishesh <vgvishesh2008@gmail.com>
2023-12-24 16:45:30 +0530

    p12

--- src/App.tsx
+++ src/App.tsx
@@ -71,9 +71,9 @@ const App: React.FC = () => {
   };
 
   return (
-    <div className="container mx-auto p-4 overflow-auto">
+    <div className="container mx-auto p-4">
       <div className="flex flex-col lg:flex-row justify-center items-start lg:space-x-8 space-y-8 lg:space-y-0">
-        <div>
+        <div className="flex-1 overflow-auto">
           <KeyValueForm keyValuePairs={keyValuePairs} onChange={handleKeyValueChange} />
           <div className="my-8">
             <PromptField promptText={promptText} keyValuePairs={keyValuePairs} onChange={handlePromptChange} />
@@ -82,7 +82,7 @@ const App: React.FC = () => {
             Submit
           </button>
         </div>
-        <div className="w-full lg:w-auto p-4 border border-gray-300 rounded" style={{ maxHeight: '700px', overflowY: 'auto', backgroundColor: 'black', color: 'white' }}>
+        <div className="flex-1 p-4 border border-gray-300 rounded max-h-[700px] overflow-y-auto bg-black text-white">
           <h3 className="font-bold text-lg mb-2">Response:</h3>
           {isLoading ? (
             <div className="flex justify-center items-center h-full">
